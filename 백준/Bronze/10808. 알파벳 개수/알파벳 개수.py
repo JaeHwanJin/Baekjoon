3a257d0fcd20f8ccc78b2ca6fefb777a8ade5027
Jinga <110621233+JaeHwanJin@users.noreply.github.com>
2023-01-29 15:09:32 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -1,12 +1,5 @@
-inp = input()
-word = 'abcdefghijklmnopqrstuvwxyz'
-words = {}
-result = ''
-for i in word :
-    words[i] = 0
-for i in range(len(inp)) :
-    if inp[i] in words :
-        words[inp[i]] += 1
-for i in words.values() :
-    result += str(i) + ' '
-print(result)
+n = list(input())
+lst = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for i in range(len(lst)) :
+    y = n.count(lst[i])
+    print(y, end=' ')

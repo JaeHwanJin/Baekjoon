9d5efe881b213335b2c4d2cd4ce15565cf18be46
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-03-09 01:13:01 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 640 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,13 +1,22 @@
-sosu = [0 for i in range(10001)]
-sosu[1] = 1
-for i in range(2, 98):
-    for j in range(i * 2, 10001, i):
-        sosu[j] = 1
-t = int(input())
-for i in range(t):
-    a = int(input())
-    b = a // 2
-    for j in range(b, 1, -1):
-        if sosu[a - j] == 0 and sosu[j] == 0:
-            print(j, a - j)
+# 소수인지 확인하는 함수
+def prime(n):
+    if n == 1:
+        return False
+    # n이 n제곱은 이하의 수로 나누어 떨어지지 않으면 그 이상의 수로도 나누어 떨어지지 않는다.
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+T = int(input())
+for tc in range(T):
+    num = int(input())
+    pn1, pn2 = num // 2, num // 2   # num을 반으로 나누고 각 각 +1 -1 해주면서 두 수가 소수인지 확인
+    while pn1 > 0:
+        if prime(pn1) and prime(pn2):
+            print(pn1, pn2)
             break
+        else:
+            pn1 -= 1
+            pn2 += 1

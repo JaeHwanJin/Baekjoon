cce8b2b82abd12c96697ec251c76c1718684d225
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-03-28 17:51:36 +0900

    [Silver IV] Title: 가로수, Time: 3848 ms, Memory: 41872 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -1,24 +1,16 @@
-def gcd(num1, num2):  # 최대공약수를 구하는 함수
-    a, b = num1, num2
-    while a != 0:
-        b = b % a
-        a, b = b, a
-    return b
-
+import sys
+import math
 
 N = int(input())
-arr = [int(input()) for _ in range(N)]
+arr = [int(input()) for i in range(N)] # 심겨져있는 가로수
+arr2 = [] # 심겨져있는 가로수들의 거리
 
-far = list()
-for i in range(len(arr) - 1):
-    far.append(arr[i + 1] - arr[i])
+for i in range(1,len(arr)):
+    arr2.append(arr[i] - arr[i-1])
 
-sfar = list(set(far))
-x = sfar[0]
-for i in range(len(sfar) - 1):
-    x = gcd(x, sfar[i + 1])
-    
-result = 0
-for j in far:
-    result += j // x - 1
-print(result)
+d = math.gcd(*arr2) # 심겨져 있는 가로수들의 거리의 최대공약수
+cnt = 0
+
+for j in arr2:
+    cnt += (j // d) -1
+print(cnt)

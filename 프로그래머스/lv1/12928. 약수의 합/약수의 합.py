389393b103fd7f4723c7849c09e4001a198d6d6d
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-05-30 23:59:23 +0900

    [level 1] Title: 약수의 합, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -1,8 +1,10 @@
 def solution(n):
     answer = 0
+    sum = 0
+    
     for i in range(1, n+1):
-        if n // i == 1:
-            print(i)
-            answer += i
-    print(answer)
+        if n % i == 0:
+            sum += i
+            answer = sum
+    
     return answer

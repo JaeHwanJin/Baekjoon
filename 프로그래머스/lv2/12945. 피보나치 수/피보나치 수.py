a9daa3e7c052e6ee1f4a17f3a81c5acad6aabbb1
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-07-26 22:36:49 +0900

    [level 2] Title: 피보나치 수, Time: 609.14 ms, Memory: 441 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,7 +1,8 @@
+
 def solution(n):
-    fibo = [0, 1]
+    answer = [0] *100001
+    answer[0] = 0
+    answer[1] = 1
     for i in range(2, n+1):
-        fibo.append(fibo[i-1] + fibo[i-2])
-            
-    answer = fibo[-1]%1234567
-    return answer
+        answer[i] = sum(answer[i-2:i])
+    return answer[n] % 1234567

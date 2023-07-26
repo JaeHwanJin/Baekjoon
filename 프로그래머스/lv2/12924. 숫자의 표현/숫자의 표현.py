c2fad872568bb0653dd573270758e00216508587
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-07-26 22:04:08 +0900

    [level 2] Title: 숫자의 표현, Time: 5.74 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,27 +1,18 @@
-# def solution(n):
-#     answer = 0
-#     num = 0
-#     SUM = 0
-#     while True:
-#         num+=1
-#         for i in range(num, n+1):
-#             SUM += i
-#             if SUM == n:
-#                 answer+=1
-#                 break
-#         SUM = 0
-#         if num == n:
-#             return answer
-#     return answer
 def solution(n):
-    count = 0                      
-    for i in range(1, n+1):         # 예시의 `15=15`도 있기 때문에 n+1 까지 반복문 실행
-        sumN = 0                     
-        for j in range(i, n+1):     # i값을 시작으로 반복문 실행
-            sumN += j               # i값부터 계속해서 값을 더해준다
-            if sumN == n:           # 더한 값이(sumN)이 n과 같다면 count +1, break
-                count += 1          
+    answer = 1
+    start = 1
+    while True:
+        SUM = 0
+        for i in range(start, n):
+            SUM += i
+            if SUM == n:
+                answer += 1
                 break
-            if sumN > n:            # 더한 값(sumN)이 n보다 크다면 계산할 필요가 없음
+            if SUM > n:
                 break
-    return count
+        start += 1
+        if start >= n:
+            break
+    return answer
+
+

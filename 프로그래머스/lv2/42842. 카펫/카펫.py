50c1456e46b69e0ef16966cc4538e696ef9447e8
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-07-28 00:18:39 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,18 +1,7 @@
 def solution(brown, yellow):
-    
-    answer = []
-    
-    yellow_x = 0
-    yellow_y = 0
-    
-    for i in range(1, yellow+1) :
-        if yellow % i == 0 :
-            yellow_x = int(yellow/i)
-            yellow_y = i
-            if yellow_x*2 + yellow_y*2 + 4 == brown :            
-                answer.append(yellow_x+2)
-                answer.append(yellow_y+2)
-                
-                return sorted(answer, reverse = True)
-    
-    return answer
+    total = brown + yellow
+    for i in range(1, int(total ** 0.5) + 1):
+        if total % i == 0:
+            div = total // i
+            if (i - 2) * (div - 2) == yellow:
+                return [max(i, div), min(i, div)]

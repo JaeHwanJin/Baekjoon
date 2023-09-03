a8a78ac3744ffec6d4f1a49ca434f525501634d7
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-09-03 19:52:59 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 26.01 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,15 +1,5 @@
+
 function solution(arr)
-{   
-    var stack =[arr[0]];
-    
-    for (let i=1; i<arr.length; i++){
-        if(stack[stack.length-1] ==arr[i]){
-            continue;
-        }
-        else{
-            stack.push(arr[i])
-        }
-    }
-    
-    return stack;
+{
+    return arr.filter((val,index) => val != arr[index+1]);
 }

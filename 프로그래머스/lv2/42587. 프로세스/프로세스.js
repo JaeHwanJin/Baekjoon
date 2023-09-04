2f160d2186482f1ca0ae14eafbd3b2b40fb45cf4
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-09-04 20:55:20 +0900

    [level 2] Title: 프로세스, Time: 5.96 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.js
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.js
@@ -1,18 +1,21 @@
 function solution(priorities, location) {
     var answer = 0;
-    var maxPriority = Math.max(...priorities);
-    
+    var newPriorities = priorities.map((priority, index) => ({priority, index}));
+    console.log(newPriorities)
     while (true) {
-        var current = priorities.shift();
-        
-        if (current === maxPriority) {
-            answer++;
-            if (location === 0) return answer;
-            maxPriority = Math.max(...priorities);
-        } else {
-            priorities.push(current);
+        var current = newPriorities.shift();
+        console.log(current)
+        var check = newPriorities.some(newPriority => newPriority.priority > current.priority)
+        if(check){
+            newPriorities.push(current);
         }
-        
-        location = (location - 1 + priorities.length) % priorities.length;
+        else if(!check && current.index === location){
+                        answer++;
+
+            return answer;
+        }
+        else{
+            answer++;
+        }
     }
 }

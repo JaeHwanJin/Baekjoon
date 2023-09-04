0de425fe3101fb679179a465dc67d648c32f9669
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-09-04 09:22:45 +0900

    [level 2] Title: 기능개발, Time: 5.25 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,16 +1,20 @@
-function solution(progresses, speeds) {
-    let answer = [0];
-    let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
-    let maxDay = days[0];
-    console.log(days)
-    for(let i = 0, j = 0; i< days.length; i++){
-        if(days[i] <= maxDay) {
-            answer[j] += 1;
-        } else {
-            maxDay = days[i];
-            answer[++j] = 1;
+function solution(progresses, speeds){
+    var answer = []
+    var newProgresses = progresses.map((progress, index, day) => ({ progress, index,day : Math.ceil((100-progress)/speeds[index])}));
+    console.log(newProgresses)
+    var maxDay = newProgresses[0].day
+    var cnt = 0;
+    for(let i=0; i<newProgresses.length; i++){
+        if(newProgresses[i].day > maxDay){
+            maxDay = newProgresses[i].day
+            answer.push(cnt)
+            cnt = 0
+        }
+        if(newProgresses[i].day <= maxDay){
+            cnt++;
         }
     }
-
+    answer.push(cnt)
+    
     return answer;
 }

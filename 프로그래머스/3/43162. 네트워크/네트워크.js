cba7eadd9cf76abef6103750f6e6b530e85c2733
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-10-16 00:38:43 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,20 +1,19 @@
 function solution(n, computers){
     var answer = 0;
-    var visited = [false]
-    function dfs(start){
-        visited[start] = true;
+    var visited = Array(n).fill(0)
+    function DFS(start){
+        visited[start] = 1;
         for(let i=0; i<computers[start].length; i++){
-            if(computers[start][i] == true && visited[i] != true){
-                dfs(i)    
+            if(computers[start][i]==1 && visited[i]==0){
+                DFS(i)
             }
         }
     }
-    
     for(let j=0; j<computers.length; j++){
-        if(visited[j] != true){
-            dfs(j);
+        if(visited[j]==0){
+            DFS(j)       
             answer += 1;
         }
     }
-    return answer
+    return answer 
 }

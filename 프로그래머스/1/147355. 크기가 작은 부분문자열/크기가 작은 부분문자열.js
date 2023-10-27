4de8d4d54853f91eae76f69dbd468080e5ebe2ef
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-10-27 10:02:19 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,13 +1,10 @@
 function solution(t, p) {
     var answer = 0;
-    var lst = [];
     for(let i=0; i<t.length-p.length+1; i++){
-        lst.push(t.slice(i,i+p.length));
-    }
-    for(let j=0; j<lst.length; j++){
-        if(parseInt(lst[j])<=parseInt(p)){
+        if(parseInt(t.slice(i,i+p.length))<=parseInt(p)){
             answer++;
         }
     }
+
     return answer;
 }

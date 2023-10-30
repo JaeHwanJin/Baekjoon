9f55b81f292ef7c3db24fbf42fceaaba6db2974a
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-10-30 13:15:53 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 12.83 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -2,19 +2,17 @@ function solution(s) {
     let stack = [];
     let ans = [];
     
-    for(let i = 0; i < s.length; i++){
+    for(let i=0; i<s.length; i++){
         if(!stack.includes(s[i])){
-            ans.push(-1);
-            stack.push(s[i]);
+            stack.push(s[i])
+            ans.push(-1)
             continue;
         }
-        
         if(stack.includes(s[i])){
-            ans.push(stack.length - stack.lastIndexOf(s[i]));
-            stack.push(s[i]);
+            ans.push(Math.abs(stack.lastIndexOf(s[i])-i))
+            stack.push(s[i])
             continue;
         }
     }
-    
     return ans;
 }

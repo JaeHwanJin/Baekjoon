053c738597da6c2dfdbdce3a7c39797d99bfc1d7
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-11-01 09:59:27 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.57 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,14 @@
+function solution(numbers) {
+    var answer = [];
+    for(let i=0; i<numbers.length; i++){
+        for(let j=i+1; j<numbers.length; j++){
+            if(answer.includes(numbers[i]+numbers[j])){
+                continue;
+            }else{
+                answer.push(numbers[i]+numbers[j])
+            }
+        }
+    }    
+    answer = answer.sort((a,b)=> a-b)
+    return answer;
+}

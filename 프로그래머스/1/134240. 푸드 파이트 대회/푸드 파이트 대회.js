69dff395175d5411f77d8cd54a352764ef4f84f9
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-11-01 09:54:24 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,15 +1,9 @@
 function solution(food) {
     var answer = '';
-    var list = [];
-    var newList = [];
-    for(let i=0; i<food.length; i++){
-        if(food[i]/2>=1){
-            const idx = i.toString();
-            list.push(idx.repeat(Math.floor(food[i]/2)));
-        }
+    var list = "";
+    for(let i=1; i<food.length; i++){
+        list += (i.toString().repeat(Math.floor(food[i]/2)));
     }
-    list = list.join("");
-    newList = [...list].reverse().join("");
-    answer =list+"0"+newList;
+    answer = list + "0" + [...list].reverse().join("");
     return answer;
 }

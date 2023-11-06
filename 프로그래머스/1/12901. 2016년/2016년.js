2c7c63b13bd737f5b868d69bd820b2386cdfc05f
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-11-06 14:55:30 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,16 +1,16 @@
 function solution(a, b) {
-    var days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
-    var day = ["FRI","SAT","SUN","MON","TUE","WED","THU"]
+    var days = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+    var day = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
     var sum = 0;
     for(let i=0; i<a; i++){
         sum+= days[i]
     }
     sum += b
-    if(a<3){
-        var answer = day[sum%7-1]    
-    }
-    else{
+    // if(a<3){
+        // var answer = day[sum%7-1]    
+    // }
+    // else{
         var answer = day[sum%7]
-    }
+    // }
     return answer;
 }

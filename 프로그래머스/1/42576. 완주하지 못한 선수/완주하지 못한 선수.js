ac94b685472daa6f667a71996c6437cab91d324d
Jinga <110621233+Jinga02@users.noreply.github.com>
2023-11-08 11:14:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 90.71 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,18 +1,10 @@
 function solution(participants, completion) {
-    var answer = {};
-    for(participant of participants){
-        if(answer[participant] > 0){
-            answer[participant] += 1;    
-        }else{
-            answer[participant] = 1;    
-        }
+    participants.sort()
+    completion.sort()
+    for(let i=0; i<participants.length; i++){
+        if(participants[i] !== completion[i]){
+            return participants[i]
+        }        
     }
-    for(winner of completion){
-        answer[winner] -= 1;
-    }
-    for(loser in answer){
-        if(answer[loser] > 0){
-            return loser
-        }
-    }
+
 }
